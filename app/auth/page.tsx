b725c1774eec17824ff3f1e9fff597f86419f553
Lostjerome <naronkrach@gmail.com>
2025-02-13 11:43:55 +0700

    Implement user authentication check in AuthPage and redirect unauthenticated users

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -1,10 +1,20 @@
 import { signInWithGoogle } from "@/actions/auth";
 import CtaButton from "@/components/ctaButton";
 import Footer from "@/components/footer";
+import { createClient } from "@/utils/supabase/server";
 import { Heart, Key } from "lucide-react";
+import { redirect } from "next/navigation";
 import React from "react";
 
-export default function AuthPage() {
+export default async function AuthPage() {
+  const supabase = await createClient();
+
+  // Check if the user is authenticated
+  const session = await supabase.auth.getUser();
+  if (session.data?.user) {
+    return redirect("/");
+  }
+
   return (
     <div className="relative flex flex-col items-center justify-center h-screen space-y-6">
       <div className="flex items-center justify-center space-x-2">
@@ -13,9 +23,7 @@ export default function AuthPage() {
           Love<span className="font-semibold">Letter</span>
         </h1>
       </div>
-      <CtaButton
-        onClick={signInWithGoogle}
-      >
+      <CtaButton onClick={signInWithGoogle}>
         <Key />
         Continue with google
       </CtaButton>
