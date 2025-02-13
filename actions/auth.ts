a71540de3f2f54589988b4323aac488c27337fca
Lostjerome <naronkrach@gmail.com>
2025-02-13 12:19:39 +0700

    Update authentication callback URL to use production environment

--- actions/auth.ts
+++ actions/auth.ts
@@ -7,7 +7,7 @@ import { redirect } from "next/navigation";
 const signInWith = (provider: Provider) => async () => {
   const supabase = await createClient();
 
-  const auth_callback_url = `${process.env.SITE_URL}/auth/callback`;
+  const auth_callback_url = `https://sendloveletter.vercel.app/auth/callback`;
 
   const { data, error } = await supabase.auth.signInWithOAuth({
     provider,
